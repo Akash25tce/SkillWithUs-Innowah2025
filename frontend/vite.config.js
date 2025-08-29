3571554dab8e52546ac39720d391b1f165668722
Akash25tce <akash25aswinalr@gmail.com>
2025-08-29 07:17:28 +0530

    new3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,9 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// Get the branch name from the AWS Amplify environment variable
+const branchName = process.env.AWS_BRANCH;
+// Construct the base URL dynamically.
+// The structure will be /<branch-name>/<path-to-frontend>/
+const BASE_URL = `/${branchName}/frontend/`;
+
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),],
-})
+    tailwindcss(),
+  ],
+  base: BASE_URL,
+});
