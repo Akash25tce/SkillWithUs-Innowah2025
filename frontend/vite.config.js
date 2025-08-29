df54ef34f7357de6dff49078846e56f945f74559
Akash25tce <akash25aswinalr@gmail.com>
2025-08-29 07:12:25 +0530

    new2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
-  ],
-  // Add the base path for deployment
-  base: '/SkillWithUs-Innowah2025-f76f6144ba955bd01b6eb4ce11c2115646b03ba1/frontend/'
+    tailwindcss(),],
 })
