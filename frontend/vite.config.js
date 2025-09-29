065c3e7880ff54cd5b2485f8876b8c73f7d1569b
Akash25tce <117701979+Akash25tce@users.noreply.github.com>
2025-09-29 10:12:05 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    allowedHosts: [
-      "5e581373-ab91-4b91-b49f-0b5dbb2e6f59-00-1zh61zhqp0g8s.pike.replit.dev"
-    ],
-    host: true,  // makes it accessible from external hosts
-    port: 5173   // optional: you can set a port if needed
-  }
-})
+  plugins: [
+    react(),
+    tailwindcss(),],
